bd527ce007152f02b4a3d4ffd1c10ea9bf6c6775
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2025-11-27 12:13:25 -0300

    Add movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,29 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const VEL = 100
+
+var move = 0
 
 func _ready():
 	# Called every time the node is added to the scene.
-	# Initialization here
+	set_process(true)
 	pass
 
 func _process(delta):
-	pass
+	# zerando meu dir no início desse frame
+	move = 0
+	
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	
+	# se nenhum, move = 0 
+	# se right, move = 1
+	# se left, move = -1
+	# se os dois, move = 1 - 1 = 0
+	if right:
+		move += 1
+	
+	if left:
+		move -= 1
+	
+	translate(Vector2(1, 0) * VEL * move * delta)
